eaf7898ee78b5ea3c087931ef1864e68943b2d49
Mayuri Salunke <73269467+mayurims@users.noreply.github.com>
2021-09-20 15:46:56 +0800

    Update engineers.sql

--- engineers.sql
+++ engineers.sql
@@ -22,10 +22,10 @@ CREATE TABLE IF NOT EXISTS `engineers` (
   `engineer_name` varchar(64) NOT NULL,
   `engineer_teleid` varchar(20) NOT NULL,
   `engineer_email` varchar(20) NOT NULL,
-  `engineer_completedCourses` varchar(65535),
-  `engineer_enrolledCourses` varchar(65535),
-  `engineer_assignedCourses` varchar(65535),
-  `engineer_signedupCourses` varchar(65535),
+  `engineer_completedCourses` int(11),
+  `engineer_enrolledCourses` int(11),
+  `engineer_assignedCourses` int(11),
+  `engineer_signedupCourses` int(11),
 
   PRIMARY KEY (`engineer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -35,10 +35,10 @@ CREATE TABLE IF NOT EXISTS `engineers` (
 --
 
 INSERT INTO `engineers` (`engineer_id`, `engineer_pwd`, `engineer_name`, `engineer_teleid`, `engineer_email`, `engineer_completedCourses`, `engineer_enrolledCourses`, `engineer_assignedCourses`, `engineer_signedupCourses`) VALUES
-(1, 'password100', 'John Doe', '@abcd', 'johndoe@gmail.com', 'C1,C2,C3', 'C4,C5', 'C6,C7,C8', 'C9'),
-(2, 'password101', 'Mary Doe', '@efgh', 'marydoe@gmail.com', 'C1,C2,C3', 'C4,C5', 'C6,C7,C8,C9', 'C10,C11'),
-(3, 'password102', 'Jason Doe', '@ijkl', 'jasondoe@gmail.com', 'C1,C2,C3', 'C4,C5', 'C6,C7,C8', 'C9'),
-(4, 'password103', 'Selena Doe', '@mnop', 'selenadoe@gmail.com', 'C1,C2,C3', 'C4,C5', 'C6,C7,C8,C9', 'C10,C11'),
-(5, 'password104', 'May Smith', '@may123', 'maysmith@gmail.com', 'C1,C2,C3', 'C4,C5', 'C6,C7,C8', 'C9'),
-(6, 'password105', 'Jack Smith', '@jack456', 'jacksmith@gmail.com', 'C1,C2,C3', 'C4,C5', 'C6,C7,C8,C9', 'C10,C11');
+(1, 'password100', 'John Doe', '@abcd', 'johndoe@gmail.com', '1,2,3', '4,5', '6,7,8', '9'),
+(2, 'password101', 'Mary Doe', '@efgh', 'marydoe@gmail.com', '1,2,3', '4,5', '6,7,8,9', '10,11'),
+(3, 'password102', 'Jason Doe', '@ijkl', 'jasondoe@gmail.com', '1,2,3', '4,5', '6,7,8', '9'),
+(4, 'password103', 'Selena Doe', '@mnop', 'selenadoe@gmail.com', '1,2,3', '4,5', '6,7,8,9', '10,11'),
+(5, 'password104', 'May Smith', '@may123', 'maysmith@gmail.com', '1,2,3', '4,5', '6,7,8', '9'),
+(6, 'password105', 'Jack Smith', '@jack456', 'jacksmith@gmail.com', '1,2,3', '4,5', '6,7,8,9', '10,11');
 COMMIT;
