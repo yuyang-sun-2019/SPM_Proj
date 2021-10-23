243d59fda6d7cc531f3ab7fd9435df016ec66448
Mayuri Salunke <73269467+mayurims@users.noreply.github.com>
2021-10-23 13:43:24 +0800

    Update person.py

--- flask/person.py
+++ flask/person.py
@@ -80,4 +80,4 @@ def find_by_id(id):
 if __name__ == '__main__':
     print("This is flask for " + os.path.basename(__file__) +
           ": manage person details ...")
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
