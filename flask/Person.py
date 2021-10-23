fc267ec6101fbd8d8533ac53912834334c244fa0
mayurims <mayurims@users.noreply.github.com>
2021-10-23 16:35:19 +0800

    Update Person.py

--- flask/Person.py
+++ flask/Person.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import os
-from os import environ
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:''' + \
@@ -20,23 +18,52 @@ class Person(db.Model):
     __tablename__ = 'Person'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(15), unique=True)
+    name = db.Column(db.String(15))
     email = db.Column(db.String(50), unique=True)
-    pwd = db.Column(db.String(80))
+    pwd = db.Column(db.String(80), unique=True)
 
-    def __init__(self,id,name,email,pwd):
-        self.id = id
-        self.name: name
-        self.email: email
-        self.pwd:pwd
+    __mapper_args__ = {
+        'polymorphic_identity': 'person'
+    }
 
-    def json(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'email': self.email,
-            'pwd': self.pwd
-        }
+    def to_dict(self):
+        """
+        'to_dict' converts the object into a dictionary,
+        in which the keys correspond to database columns
+        """
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+
+class Engineer(db.Model):
+    __tablename__ = 'engineers'
+
+    engineer_id = db.Column(db.Integer, primary_key=True)
+    engineer_teleid = db.Column(db.String(20))
+    engineer_completed_coursesid = db.Column(db.String(64))
+    engineer_inprogress_coursesid = db.Column(db.String(64))
+    engineer_preassigned_coursesid = db.Column(db.String(64))
+    engineer_biddable_coursesid = db.Column(db.String(64))
+
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'engineers'
+    }
+
+    def to_dict(self):
+        """
+        'to_dict' converts the object into a dictionary,
+        in which the keys correspond to database columns
+        """
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
 
 
 @app.route("/person")
@@ -47,7 +74,7 @@ def get_all_person():
             {
                 "code": 200,
                 "data": {
-                    "Person": [person.json() for person in people]
+                    "Person": [person.to_dict() for person in people]
                 }
             }
         )
@@ -66,7 +93,7 @@ def find_by_id(id):
         return jsonify(
             {
                 "code": 200,
-                "data": person.json()
+                "data": person.to_dict()
             }
         )
     return jsonify(
@@ -77,7 +104,44 @@ def find_by_id(id):
     ), 404
 
 
+@app.route("/engineer")
+def get_all_engineer():
+    engineers = Engineer.query.all()
+    if len(engineers):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "Person": [eng.to_dict() for eng in engineers]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Engineer not found."
+        }
+    ), 404
+
+@app.route("/engineer/<string:engineer_id>")
+def get_engineer_byID(engineer_id):
+    engineer = Engineer.query.filter_by(engineer_id=engineer_id).first()
+    if engineer:
+        return jsonify(
+            {
+                "code": 200,
+                "data": engineer.to_dict()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Engineer not found."
+        }
+    ), 404
+
+
+
+
 if __name__ == '__main__':
-    print("This is flask for " + os.path.basename(__file__) +
-          ": manage person details ...")
     app.run(host='0.0.0.0', port=5002, debug=True)
