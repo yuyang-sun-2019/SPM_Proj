c259081bc53eb11fb636803c3f56e105d678408f
yuyang-sun-2019 <yuyang.sun.2019@sis.smu.edu.sg>
2021-11-02 11:47:23 +0800

    deleted irrelevant

--- flask/Quiz class.py
+++ flask/Quiz class.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root' + \
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
                                         '@localhost:3306/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
