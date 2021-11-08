501e401977cd0eb8834c565e6184eb085720d405
skykeyang <72445808+skykeyang@users.noreply.github.com>
2021-11-09 04:35:17 +0800

    Update Quiz.py

--- flask/Quiz.py
+++ flask/Quiz.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:''' + \
-                                        '@localhost:3306/mydb'
+                                        'spmdb1.c4n4efeq3vyp.us-east-1.rds.amazonaws.com:3306/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}
