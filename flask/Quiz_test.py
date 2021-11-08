b111423b8c37f8c41a71bb5eada0f7f775d279de
jingling-ho <89132854+jingling-ho@users.noreply.github.com>
2021-11-08 20:03:11 +0800

    Update Quiz_test.py

--- flask/Quiz_test.py
+++ flask/Quiz_test.py
@@ -1,48 +1,104 @@
 import unittest
 
-from Quiz import Quiz, quiz_qn_ans
+import unittest
 
+from Quiz import Quiz, quiz_qn_ans, quiz_take 
 
-# Done by Ho Jing Ling
+###Done by Ho Jing Ling
+##Test if there is a quiz 
 class TestQuiz(unittest.TestCase):
-    c1 = Quiz(quiz_id='C101-L1-Quiz', quiz_title='C101-L1', quiz_duration='00:15:00', FK_trainer_id='761')
+    q1 = Quiz(quiz_id='C101-L1-Quiz', quiz_title='C101-L1', quiz_duration ='00:15:00', FK_trainer_id= '761')
+    
+    
     def test_to_dict(self):
-        self.assertEqual(self.c1.to_dict(), {
+        self.assertEqual(self. q1.to_dict(), {
             'quiz_id': 'C101-L1-Quiz',
             'quiz_title': 'C101-L1',
             'quiz_duration': '00:15:00',
-            'FK_trainer_id': '761'
+            'FK_trainer_id' : '761',
         }
         )
 
-# # Done by Ho Jing Ling
+    # Check if the Quiz Id is in the correct format
+    def test_quiz_id(self):
+        self.assertIn(self.q1.to_dict()['quiz_id'].split('-')[2][0], 'Q')
+
+
+
+###Done by Hp Jing Ling
+## Test for the presence of quiz_qn_as
 class Testquiz_qn_ans(unittest.TestCase):
-    cc1 = quiz_qn_ans(quiz_qn_id='MCQ_Qn1')
+    cc1 = quiz_qn_ans(quiz_qn_id='C101-L1-Quiz',  
+            MCQ_Qn1= 'What is the  lesson?',
+            MCQ_Qn1_A='SPM',
+            MCQ_Qn1_B='ESM', 
+            MCQ_Qn1_C='ESD', 
+            MCQ_Qn1_D='SPM', 
+            MCQ_Qn1_Ans='1', 
+            MCQ_Qn1_Score='1', 
+            MCQ_Qn2='What is the most favourite color?', 
+            MCQ_Qn2_A='red', 
+            MCQ_Qn2_B='pink', 
+            MCQ_Qn2_C='yellow', 
+            MCQ_Qn2_D='blue', 
+            MCQ_Qn2_Ans='blue', 
+            MCQ_Qn2_Score='1', 
+            TandF_Qn1='I like starbucks?', 
+            TandF_Qn1_Ans='True', 
+            TandF_Qn1_Score='1', 
+            TandF_Qn2='I do not like strabucks?',
+            TandF_Qn2_Ans='False',
+            TandF_Qn2_Score='1'
+)
     def test_to_dict(self):
-        self.assertTrue(self.cc1.to_dict(), {
-            'quiz_qn_id': 'MCQ_Qn1',
-            'quiz_qn_id': 'MCQ_Qn1_A',
-            'quiz_qn_id': 'MCQ_Qn1_B',
-            'quiz_qn_id': 'MCQ_Qn1_C',
-            'quiz_qn_id': 'MCQ_Qn1_D',
-            'quiz_qn_id': 'MCQ_Qn1_Ans',
-            'quiz_qn_id': 'MCQ_Qn1_Score',
-            'quiz_qn_id': 'MCQ_Qn2',
-            'quiz_qn_id': 'MCQ_Qn2_A',
-            'quiz_qn_id': 'MCQ_Qn2_B',
-            'quiz_qn_id': 'MCQ_Qn2_C',
-            'quiz_qn_id': 'MCQ_Qn2_D',
-            'quiz_qn_id': 'MCQ_Qn2_Ans',
-            'quiz_qn_id': 'MCQ_Qn2_Score',
-            'quiz_qn_id': 'TandF_Qn1',
-            'quiz_qn_id': 'TandF_Qn1_Ans',
-            'quiz_qn_id': 'TandF_Qn1_Score',            
-            'quiz_qn_id': 'TandF_Qn2',
-            'quiz_qn_id': 'TandF_Qn2_Ans',
-            'quiz_qn_id': 'TandF_Qn2_Score'            
+        self.assertEqual(self.cc1.to_dict(), {
+            
+            'quiz_qn_id': 'C101-L1-Quiz',
+            'MCQ_Qn1': 'What is the  lesson?',
+            'MCQ_Qn1_A':'SPM',
+            'MCQ_Qn1_B':'ESM', 
+            'MCQ_Qn1_C':'ESD', 
+            'MCQ_Qn1_D':'SPM', 
+            'MCQ_Qn1_Ans':'1', 
+            'MCQ_Qn1_Score':'1', 
+            'MCQ_Qn2':'What is the most favourite color?', 'MCQ_Qn2_A':'red', 
+            'MCQ_Qn2_B':'pink', 
+            'MCQ_Qn2_C':'yellow', 
+            'MCQ_Qn2_D':'blue', 
+            'MCQ_Qn2_Ans':'blue', 
+            'MCQ_Qn2_Score':'1', 
+            'TandF_Qn1':'I like starbucks?', 
+            'TandF_Qn1_Ans':'True', 
+            'TandF_Qn1_Score':'1', 
+            'TandF_Qn2':'I do not like strabucks?', 
+            'TandF_Qn2_Ans':'False',
+            'TandF_Qn2_Score':'1'
 
         }
         )
+
+
+# Done By Goh Xue Qi
+class TestQuizTake(unittest.TestCase):
+    t1 = quiz_take(quiz_take_id='001-C101-L1', quiz_score=2)
+    
+    
+    def test_to_dict(self):
+        self.assertEqual(self.t1.to_dict(), {
+            'quiz_take_id': '001-C101-L1',
+            'quiz_score': 2,
+        }
+        )
+
+  # Test scores are greater or equal to 0
+    def test_q_score(self):
+        self.assertGreater(self.t1.to_dict()['quiz_score'], -1)
+
+    # Check if the quiz_take_id is in the correct format
+    def test_quiz_id(self):
+        self.assertIn(self.t1.to_dict()['quiz_take_id'].split('-')[2][0], 'L')
+
+    
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()   
